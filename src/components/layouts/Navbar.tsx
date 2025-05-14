4e366ccb86d18e2382c872b86faf634255376c3d
Galang Arsandy <galangarsandy@gmail.com>
2025-05-15 04:39:44 +0700

    feat: update Navbar layout for improved responsiveness and fixed positioning

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -6,8 +6,8 @@ import ReownConnectButton from "../ui/ReownConnectButton";
 
 export default function Navbar() {
     return (
-        <header className=" max-w-7xl mx-auto mt-4">
-            <div className="flex items-center justify-between px-8 bg-gray-100 h-[88px] rounded-full shadow-md py-4 ">
+        <header className="w-full max-w-7xl px-4 lg:px-8 mx-auto pt-4 fixed top-0 z-50 left-1/2 transform -translate-x-1/2">
+            <div className="flex items-center justify-between px-7 bg-gray-100 h-[88px] rounded-full py-4 ">
                 <Image src={"/pawfund.svg"} alt="PawFund Logo" width={152} height={0} />
                 <div className="flex items-center gap-4">
                     <Button variant={"navbar"} className="w-14 h-14 p-0">
